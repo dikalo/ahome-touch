08a299631533daddacd2fc6f91750feed9751f48
eemi2010 <jazzmatadazz@gmail.com>
2014-10-06 02:09:31 +0200

    adds event handlers to navigation view

---
+++ ahome-touch/src/com/ait/toolkit/sencha/touch/client/events/navigation/PopHandler.java
@@ -0,0 +1,7 @@
+package com.ait.toolkit.sencha.touch.client.events.navigation;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface PopHandler extends EventHandler {
+	public void onPop(PopEvent event);
+}
