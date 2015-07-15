97ba4dec2921769fc73828b3c221d0bc788fac91
eemi210 <alainekambi@alainekmacbook2.fritz.box>
2015-07-15 02:15:57 +0200

    Fixes toggle event handler

--- ahome-touch/src/main/java/com/ait/toolkit/sencha/touch/client/events/toggle/ToggleChangeEvent.java
+++ ahome-touch/src/main/java/com/ait/toolkit/sencha/touch/client/events/toggle/ToggleChangeEvent.java
@@ -5,44 +5,44 @@ import com.google.gwt.event.dom.client.DomEvent.Type;
 
 public class ToggleChangeEvent {
 
-	public static String EVENT_NAME = "change";
+    public static String EVENT_NAME = "change";
 
-	private Toggle source;
-	private boolean newValue;
-	private boolean value;
+    private Toggle source;
+    private int newValue;
+    private int value;
 
-	protected ToggleChangeEvent(Toggle source, boolean newValue, boolean value) {
-		super();
-		this.source = source;
-		this.newValue = newValue;
-		this.value = value;
-	}
+    protected ToggleChangeEvent( Toggle source, int newValue, int value ) {
+        super();
+        this.source = source;
+        this.newValue = newValue;
+        this.value = value;
+    }
 
-	/**
-	 * UiBinder implementations
-	 */
-	private static Type<ToggleChangeHandler> TYPE = new Type<ToggleChangeHandler>(EVENT_NAME, null);
+    /**
+     * UiBinder implementations
+     */
+    private static Type<ToggleChangeHandler> TYPE = new Type<ToggleChangeHandler>( EVENT_NAME, null );
 
-	public static Type<ToggleChangeHandler> getType() {
-		return TYPE;
-	}
+    public static Type<ToggleChangeHandler> getType() {
+        return TYPE;
+    }
 
-	public static Type<ToggleChangeHandler> getAssociatedType() {
-		return TYPE;
-	}
+    public static Type<ToggleChangeHandler> getAssociatedType() {
+        return TYPE;
+    }
 
-	/**
-	 * @return the source
-	 */
-	public Toggle getSource() {
-		return source;
-	}
+    /**
+     * @return the source
+     */
+    public Toggle getSource() {
+        return source;
+    }
 
-	public boolean isChecked() {
-		return newValue;
-	}
+    public int getNewValue() {
+        return newValue;
+    }
 
-	public boolean wasChecked() {
-		return value;
-	}
+    public int getOldValue() {
+        return value;
+    }
 }
--- ahome-touch/src/main/java/com/ait/toolkit/sencha/touch/client/ui/Toggle.java
+++ ahome-touch/src/main/java/com/ait/toolkit/sencha/touch/client/ui/Toggle.java
@@ -1,17 +1,17 @@
 /*
- Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.ait.toolkit.sencha.touch.client.ui;
 
@@ -26,102 +26,101 @@ import com.google.gwt.event.shared.HandlerRegistration;
  */
 public class Toggle extends Slider {
 
-	@Override
-	protected native void init()/*-{
+    @Override
+    protected native void init()/*-{
 		var c = new $wnd.Ext.field.Toggle();
 		this.@com.ait.toolkit.sencha.touch.client.core.Component::configPrototype = c.initialConfig;
-	}-*/;
+    }-*/;
 
-	@Override
-	public String getXType() {
-		return XType.TOGGLE_FIELD.getValue();
-	}
+    @Override
+    public String getXType() {
+        return XType.TOGGLE_FIELD.getValue();
+    }
 
-	@Override
-	protected native JavaScriptObject create(JavaScriptObject config) /*-{
+    @Override
+    protected native JavaScriptObject create( JavaScriptObject config ) /*-{
 		return new $wnd.Ext.field.Toggle(config);
-	}-*/;
+    }-*/;
 
-	public Toggle(ToggleConfig config) {
-		super(config.getJsObj());
-	}
+    public Toggle( ToggleConfig config ) {
+        super( config.getJsObj() );
+    }
 
-	public Toggle() {
-	}
+    public Toggle() {}
 
-	protected Toggle(JavaScriptObject jso) {
-		super(jso);
-	}
+    protected Toggle( JavaScriptObject jso ) {
+        super( jso );
+    }
 
-	/**
-	 * Returns the value of maxValueCls.
-	 * 
-	 * @return
-	 */
-	public native String getMaxValueCls() /*-{
+    /**
+     * Returns the value of maxValueCls.
+     * 
+     * @return
+     */
+    public native String getMaxValueCls() /*-{
 		var field = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		if (field != null) {
 			return field.getMaxValueCls();
 		}
-	}-*/;
+    }-*/;
 
-	/**
-	 * Returns the value of minValueCls.
-	 * 
-	 * @return
-	 */
-	public native String getMinValueCls() /*-{
+    /**
+     * Returns the value of minValueCls.
+     * 
+     * @return
+     */
+    public native String getMinValueCls() /*-{
 		var field = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		if (field != null) {
 			return field.getMinValueCls();
 		}
-	}-*/;
+    }-*/;
 
-	/**
-	 * Sets the value of minValueCls.
-	 * 
-	 * @param value
-	 */
-	public native void setMinValueCls(String value) /*-{
+    /**
+     * Sets the value of minValueCls.
+     * 
+     * @param value
+     */
+    public native void setMinValueCls( String value ) /*-{
 		var field = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		if (field != null) {
 			field.setMinValueCls(value);
 		}
-	}-*/;
+    }-*/;
 
-	/**
-	 * Sets the value of maxValueCls.
-	 * 
-	 * @param value
-	 */
-	public native void setMaxValueCls(String value) /*-{
+    /**
+     * Sets the value of maxValueCls.
+     * 
+     * @param value
+     */
+    public native void setMaxValueCls( String value ) /*-{
 		var field = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		if (field != null) {
 			field.setMaxValueCls(value);
 		}
-	}-*/;
+    }-*/;
 
-	/**
-	 * Toggles the value of this toggle field.
-	 */
-	public native void toggle() /*-{
+    /**
+     * Toggles the value of this toggle field.
+     */
+    public native void toggle() /*-{
 		var field = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		if (field != null) {
 			field.toggle();
 		}
-	}-*/;
+    }-*/;
 
-	public native HandlerRegistration addToggleChangeHandler(ToggleChangeHandler handler)/*-{
+    public native HandlerRegistration addToggleChangeHandler( ToggleChangeHandler handler )/*-{
 		var component = this.@com.ait.toolkit.sencha.touch.client.core.Component::getOrCreateJsObj()();
 		var fn = function(c, v, o) {
-			var comp = @com.ait.toolkit.sencha.touch.client.ui.CheckBox::new(Lcom/google/gwt/core/client/JavaScriptObject;)(c);
-			var event = @com.ait.toolkit.sencha.touch.client.events.toggle.ToggleChangeEvent::new(Lcom/ait/toolkit/sencha/touch/client/ui/Toggle;ZZ)(comp,v,o);
+			var comp = @com.ait.toolkit.sencha.touch.client.ui.Toggle::new(Lcom/google/gwt/core/client/JavaScriptObject;)(c);
+			var event = @com.ait.toolkit.sencha.touch.client.events.toggle.ToggleChangeEvent::new(Lcom/ait/toolkit/sencha/touch/client/ui/Toggle;II)(comp,v,o);
 			handler.@com.ait.toolkit.sencha.touch.client.events.toggle.ToggleChangeHandler::onToggleChange(Lcom/ait/toolkit/sencha/touch/client/events/toggle/ToggleChangeEvent;)(event);
 		};
 		var eventName = @com.ait.toolkit.sencha.touch.client.events.toggle.ToggleChangeEvent::EVENT_NAME;
 		component.addListener(eventName, fn);
 		var toReturn = @com.ait.toolkit.sencha.touch.client.events.HandlerRegistration::new(Lcom/ait/toolkit/sencha/touch/client/core/Component;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(this,eventName,fn);
 		return toReturn;
-	}-*/;
+    }-*/;
 
 }
